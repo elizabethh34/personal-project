2c8f6212749a1a91b5a1fe62471423d555406ce7
Elizabeth Hellsten <elizabeth_hellsten@shaw.ca>
2020-12-07 09:15:10 -0600

    Add styles task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 const { src, dest, series, parallel } = require('gulp');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
 
 function htmlTask() {
   return src('src/*.html')
     .pipe(dest('dist'))
 }
 
+function stylesTask() {
+  return src('src/css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write())
+    .pipe(concat('all.css'))
+    .pipe(dest('dist/css'))
+}
+
 exports.html = htmlTask;
+exports.styles = stylesTask;
